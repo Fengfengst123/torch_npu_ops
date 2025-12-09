2bc8f578424948145f65b3119c7af0345d3b18fd
dengyingxu <dengyingxu1@jd.com>
2025-12-09 16:16:41 +0800

    feat: add torch_npu_ops wrapper interface.

---
+++ ops_npu/npu_ops.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "custom_functions_npu/atb_common.h"
+
+using namespace std;
+
+namespace atb {
+
+void npu_paged_attention(const at::Tensor& query,
+                          const at::Tensor& key_cache,
+                          const at::Tensor& value_cache,
+                          int64_t num_kv_heads,
+                          int64_t num_heads,
+                          double scale_value,
+                          const at::Tensor& block_table,
+                          const at::Tensor& context_lens,
+                          at::Tensor& out);
+
+void npu_reshape_and_cache(const at::Tensor& key,
+                            const at::Tensor& value,
+                            at::Tensor& key_cache,
+                            at::Tensor& value_cache,
+                            const at::Tensor& slot_indices);
+
+void npu_flash_attention(const at::Tensor& query,
+                          const at::Tensor& key,
+                          const at::Tensor& value,
+                          const at::Tensor& mask,
+                          const at::Tensor& seq_len,
+                          const double scale_value,
+                          const int64_t num_heads,
+                          const int64_t num_kv_heads,
+                          at::Tensor& out);
+
+}  // namespace atb
---
+++ ops_npu/paged_attention_atb.cpp
@@ -0,0 +1,48 @@
+#include <acl/acl.h>
+
+#include "custom_functions_npu/atb_common.h"
+
+namespace atb {
+void npu_paged_attention(const at::Tensor& query,
+                          const at::Tensor& key_cache,
+                          const at::Tensor& value_cache,
+                          int64_t num_kv_heads,
+                          int64_t num_heads,
+                          double scale_value,
+                          const at::Tensor& block_table,
+                          const at::Tensor& context_lens,
+                          at::Tensor& out) {
+  const c10::OptionalDeviceGuard device_guard(device_of(query));
+  OpParamCache<atb::infer::PagedAttentionParam>& pagedAttentionParamCache =
+      OpParamCache<atb::infer::PagedAttentionParam>::getInstance();
+  atb::infer::PagedAttentionParam pagedparam;
+  pagedparam.headNum = num_heads;
+  pagedparam.qkScale = scale_value;
+  pagedparam.kvHeadNum = num_kv_heads;
+  pagedparam.maskType = atb::infer::PagedAttentionParam::UNDEFINED;
+  pagedparam.batchRunStatusEnable = false;
+  pagedparam.quantType = atb::infer::PagedAttentionParam::TYPE_QUANT_UNDEFINED;
+  pagedparam.outDataType = ACL_DT_UNDEFINED;
+  pagedparam.hasQuantOffset = false;
+  pagedparam.compressType =
+      atb::infer::PagedAttentionParam::COMPRESS_TYPE_UNDEFINED;
+  pagedparam.calcType = atb::infer::PagedAttentionParam::CALC_TYPE_UNDEFINED;
+  pagedparam.scaleType = atb::infer::PagedAttentionParam::SCALE_TYPE_TOR;
+  pagedparam.inputLayout = atb::infer::TYPE_BSND;
+  pagedparam.mlaVHeadSize = 0;
+
+  ParamSetter paramsetter;
+  paramsetter.Input(query, true)
+      .Input(key_cache)
+      .Input(value_cache)
+      .Input(block_table, true)
+      .Input(context_lens, true)
+      .Output(out);
+  auto opPaged = pagedAttentionParamCache.get_operation(
+      pagedparam, "PagedAttentionOperation");
+  run_atb_cmd(opPaged, paramsetter, "PagedAttentionOperation");
+
+  return;
+}
+
+}  // namespace atb
